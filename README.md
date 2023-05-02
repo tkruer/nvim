45055c5bb1c2d91a910074bfa53e6dbf60824ef8
Tyler K <tylerkruer1@gmail.com>
2023-05-02 11:35:51 -0700

    restart

---
+++ README.md
@@ -0,0 +1,9 @@
+# nvim
+
+## Todo
+
+- [] Finish setup for Buffers (they sit right above file tree rn?)
+- [] Bottom config
+- [] Float terminal
+- [] Misc LSP and lint
+- [] Telescope tools, more remaps, etc
---
+++ lua/tkszn/init.lua
@@ -0,0 +1,46 @@
+print("hey what up")
+require('packer')
+
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
+
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("mason").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+require'FTerm'.setup({
+    border = 'double',
+    dimensions  = {
+        height = 0.9,
+        width = 0.9,
+    },
+})
