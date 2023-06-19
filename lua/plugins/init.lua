241b4fb3ec37168a3991c785610d627ebea84f9d
Tyler K <tylerkruer1@gmail.com>
2023-06-19 15:51:29 -0700

    cleaned: added themes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -236,18 +236,18 @@ local default_plugins = {
     lazy = false,
     priority = 1000
   },
---  {
---    'projekt0n/github-nvim-theme',
---    lazy = false, -- make sure we load this during startup if it is your main colorscheme
---    priority = 1000, -- make sure to load this before all the other start plugins
---    config = function()
---      require('github-theme').setup({
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({
         -- ...
---      })
+      })
   
---      vim.cmd('colorscheme github_dark_high_contrast')
---    end,
---  },
+      vim.cmd('colorscheme github_dark')
+    end,
+  },
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
