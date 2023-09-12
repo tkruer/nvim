37521eb7ab9600be5463202e38f9c7ea2c0a3912
Tyler K <tylerkruer1@gmail.com>
2023-09-12 09:48:24 -0700

    redid config. s/o Drew for having a clean and simple config to follow

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = {"python", "help", "javascript", "c", "lua", "vim", "help" },
+  ensure_installed = {"python", "cpp", "javascript", "typescript", "rust", "cuda", "json", "go", "c", "lua", "vim", "help" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
