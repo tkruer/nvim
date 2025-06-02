e10ca87ca5e0b2fcf7ced400382ab856c9813804
Tyler Kruer <tylerkruer1@gmail.com>
2025-06-02 07:20:12 -0700

    updates for telescope

--- lua/tkruer/lazy/telescope.lua
+++ lua/tkruer/lazy/telescope.lua
@@ -25,6 +25,7 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+        vim.keymap.set('n', '<leader>fe', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
